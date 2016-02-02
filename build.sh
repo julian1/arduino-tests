ea52e10197214c7f97ab0b68ceba6f402193d99c
julian1 <julian.atkinson71@gmail.com>
2016-02-02 19:39:47 +1100

    assembly listing

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ avr-gcc -mmcu=atmega328p test.o -o test.elf || exit
 avr-objcopy -O ihex -R .eeprom test.elf test.hex || exit
 
 # dump assembly/symbols
-# avr-objdump -d test > test.lst
+avr-objdump -d test.elf > test.lst
 
 # /dev/ttyS0
 # avrdude -F -V -c arduino -p ATMEGA328P -P /dev/ttyACM0 -b 115200 -U flash:w:test.hex
