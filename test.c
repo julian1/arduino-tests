e61c5c55661d56f07a84c5ab8ddaf7dc6ebd5e1e
julian1 <julian.atkinson71@gmail.com>
2016-02-11 19:28:19 +1100

    cleaner example

--- test.c
+++ test.c
@@ -2,40 +2,21 @@
 #include <avr/io.h>
 #include <util/delay.h>
  
+#define BLINK_DELAY_MS 500
  
 int main (void)
 {
-  unsigned char counter;
-  /* set PORTB for output*/
-  DDRB = 0xFF;
+   /* set pin 5 of PORTB for output*/
+   DDRB |= _BV(DDB5);
  
-  while (1)
-  {
-      /* set PORTB.2 high */
-      PORTB = 0xFF;
- 
-      /* wait (10 * 120000) cycles = wait 1200000 cycles */
-      counter = 0;
-      while (counter != 50)
-      {
-        /* wait (30000 x 4) cycles = wait 120000 cycles */
-        _delay_loop_2(30000);
-        counter++;
-      }
- 
-      /* set PORTB.2 low */
-      PORTB = 0x00;
- 
-      /* wait (10 * 120000) cycles = wait 1200000 cycles */
-      counter = 0;
-      while (counter != 50)
-      {
-        /* wait (30000 x 4) cycles = wait 120000 cycles */
-        _delay_loop_2(30000);
-        counter++;
-      }
-  }
- 
-  return 1;
+   while(1) {
+      /* set pin 5 high to turn led on */
+      PORTB |= _BV(PORTB5);
+      _delay_ms(BLINK_DELAY_MS);
+     
+      /* set pin 5 low to turn led off */
+      PORTB &= ~_BV(PORTB5);
+      _delay_ms(BLINK_DELAY_MS);
+   }
 }
 
